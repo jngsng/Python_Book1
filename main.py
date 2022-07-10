306ab381c496cd5f31604f29659479e5d5fb544b
JongSeung Kim <88412551+jngsng@users.noreply.github.com>
2022-07-10 14:30:34 +0000

    pandas rename

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-a = pd.DataFrame([["환자코드","연령","성별","거주지"],
-                  ["A00301",23,"남","서울특별시"],
-                  ["D00809",53,"여","제주특별자치도"]])
+a = pd.DataFrame([[23,"남","서울특별시"],
+                  [53,"여","제주특별자치도"]],
+                  index = ["A00301","D00809"],
+                  columns = ["연령","성별","거주지"])
+
+print(a)
+print("\n")
+
+a.rename(index={"A00301":"환자1","D00809":"환자2"}, inplace=True)
+
+a.rename(columns={"연령":"AGE","성별":"GENDER","거주지":"ADDRESS"}, inplace=True)
 
 print(a)
