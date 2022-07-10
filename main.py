c8191b8c954b6dafcd59f4e6f267c5550b241016
JongSeung Kim <88412551+jngsng@users.noreply.github.com>
2022-07-10 14:42:04 +0000

    pandas loc, iloc

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ a.rename(index={"A00301":"환자1","D00809":"환자2"}, inplace=True)
 a.rename(columns={"연령":"AGE","성별":"GENDER","거주지":"ADDRESS"}, inplace=True)
 
 print(a)
+print("\n")
+
+search_1 = a.loc["환자1"]
+search_2 = a.iloc[1]
+
+print(search_1)
+print("\n")
+print(search_2)
